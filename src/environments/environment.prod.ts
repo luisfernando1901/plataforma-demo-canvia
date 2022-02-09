c169944becf59d5cd2876e039295e2dad9b01bf7
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2022-02-09 18:25:24 -0500

    Clonación de plataforma para demostración a Canvia.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   // Development url
   //base_url: 'http://localhost:3000',
   // Production url
-  base_url: 'https://comite-pecuario-sinaloa.uc.r.appspot.com',  
+  //base_url: 'https://comite-pecuario-sinaloa.uc.r.appspot.com',
+  base_url: 'https://smarty-326915.uc.r.appspot.com',
 };
