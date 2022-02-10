0801316eb21d2532404e075fd08d11175e6ded52
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2022-02-10 10:05:29 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,5 @@ export const environment = {
   // Development url
   //base_url: 'http://localhost:3000',
   // Production url
-  //base_url: 'https://comite-pecuario-sinaloa.uc.r.appspot.com',
   base_url: 'https://smarty-326915.uc.r.appspot.com',
 };
